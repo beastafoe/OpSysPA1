f6dae41ff3a45a424fc2bd0115c5077feb0226a3
Alec Swanson <alecswanson@Alecs-MacBook-Pro.local>
2017-02-05 15:11:50 -0500

    test

--- ProgrammingAssignment1.c
+++ ProgrammingAssignment1.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main()
+typedef struct
 {
-	
+  char *name = NULL;
+  int arrival = 0;
+  int burst = 0;
+} process;
+
+int main()
+{
+    FILE *fp;
+    int numProc, runTime, quantum;
+    char *sch, *token;
+    process *allProcs;
+
+    fp = fopen("processes.in", "r");
+    if(fp == NULL)
+    {
+      printf("Invalid file\n");
+      exit();
+    }
+
+    fscanf(fp, "%s", token);
+    fscanf(fp, "%s", token);
+    allProcs = malloc((int)token * sizeof(process));
+    fscanf(fp, "%s", token);
+
+    while(token != EOF)
+    {
+      printf('%s', token);
+      fscanf(fp, "%s", token);
+      if(token == 'processcount')
+      {
+        fscanf(fp, "%s", token);
+      }
+    }
 }
